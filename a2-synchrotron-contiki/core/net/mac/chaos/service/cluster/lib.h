ddc00b99c5634721dcb2e7a210c2df0a901419d5
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-03-23 09:41:34 +0100

    Refactor utility functions to a separate file

---
+++ a2-synchrotron-contiki/core/net/mac/chaos/service/cluster/lib.h
@@ -0,0 +1,9 @@
+#ifndef _CHAOS_LIB_H_
+#define _CHAOS_LIB_H_
+
+int ipow(int base, int exp);
+void reverse(char *str, int len);
+int intToStr(int x, char str[], int d);
+void ftoa(float n, char *res, int afterpoint);
+
+#endif /* _CHAOS_LIB_H_ */
