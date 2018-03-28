47332d50bb7ccd08846edaf7ea7427649c7a5a99
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-03-28 11:18:48 +0200

    Change node list len to contain all nodes in 50 node test

--- a2-synchrotron-contiki/core/net/mac/chaos/service/cluster/cluster.h
+++ a2-synchrotron-contiki/core/net/mac/chaos/service/cluster/cluster.h
@@ -7,7 +7,7 @@ extern const chaos_app_t cluster;
 extern uint8_t is_cluster_service_running;
 
 #ifndef NODE_LIST_LEN
-#define NODE_LIST_LEN 10  //describes how many nodes can become clusterheads.
+#define NODE_LIST_LEN 50  //describes how many nodes can become clusterheads.
 #endif
 
 typedef enum {
