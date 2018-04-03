47c787f6242245956b828f5bcb35800c68781653
André Samuelsson <andresamuelsson94@gmail.com>
2018-04-03 15:28:04 +0200

    Reliability result plots for halftime handin

--- a2-synchrotron-contiki/apps/chaos/max/data-analysis/Completion.R
+++ a2-synchrotron-contiki/apps/chaos/max/data-analysis/Completion.R
@@ -1,42 +1,155 @@
-working_directory <- "repos/a2-synchrotron/a2-synchrotron-contiki/apps/chaos/max/log/"
+library(plyr)       # For mdplyr, applying a table row as args to a function.
+library(functional) # For currying
+library(magrittr)   # For piping %>%
+source("utils.R")
+
+#working_directory <- "~/repos/a2-synchrotron/a2-synchrotron-contiki/apps/chaos/max/"
+working_directory <- "~/tests" # the tests folder
+#working_directory <- "~/tests/50_nodes-comp_radius_1_2018-03-27_14:57:13/50-motes-1000x1000-spread/log"
+#working_directory <- "~/tests/50_nodes-comp_radius_1_2018-03-27_14:57:13/50-motes-100x100-spread/log"
 #setwd(working_directory)
 
-log_files <- list.files(path="round", pattern="*.txt", full.names=T, recursive=FALSE)
-info <- file.info(log_files)
-log_files <- rownames(info[info$size != 0, ])
+#test_suite_name <- "50_nodes-comp_radius_1_2018-03-28_11:28:53"
+#test_suite_name <- "50_nodes-comp_radius_1_2018-03-29_11:41:46"
+test_suite_path <- paste(working_directory, "50_nodes-comp_radius_2_2018-03-29_11:42:11", sep="/")
+#test_suite_name <- "50_nodes-comp_radius_3_2018-03-29_11:42:29"
 
+#test_suite_name <-  "50_nodes-comp_radius_1_2018-03-27_14:57:13"
 
-tables <- lapply(log_files, function(path) read.table(path))
+#table100x100 <- load_all_nodes_round_data(test_path = paste(test_suite_path,"50-motes-100x100-spread", sep = "/"))
+#table500x500 <- load_all_nodes_round_data(test_path = paste(test_suite_path,"50-motes-500x500-spread", sep = "/"))
+#table1000x1000 <- load_all_nodes_round_data(test_path = paste(test_suite_path,"50-motes-1000x1000-spread", sep = "/"))
 
-#colnames(tables[[1]]) <- c("rd", "max", "fin", "node_id", "n", "cluster_id")
-#newNames <- c("rd", "max", "fin", "node_id", "n", "cluster_id")
-#tables <- lapply(tables, setNames,  newNames)
+#tables <- list(table100x100, table500x500, table1000x1000)
 
-
-table = Reduce(function(x,y) merge(x,y, all=T), tables)
-colnames(table) <- c("rd", "max", "fin", "node_id", "n", "cluster_id")
-
-
-completion_rate <- function(round, cluster) {
-  round_entries <- table[table$rd == round,]
-  entries_in_cluster <- round_entries[round_entries$cluster_id == cluster,]
+completion_rate <- function(table, round, cluster) {
+  round_entries <- table[table$round == round,]
   
-  correct_max <- max(entries_in_cluster[["node_id"]])
+  if(round %% 2 == 0) {
+    entries_in_cluster <- round_entries
+  } else {
+    entries_in_cluster <- round_entries[round_entries$cluster_id == cluster,]  
+  }
+  
+  
+  if(empty(entries_in_cluster)) {
+    return(NA)
+  }
+  
+  if(round %% 2 == 0) {
+    correct_max <- 50
+  } else {
+    correct_max <- max(entries_in_cluster[["node_id"]])
+  }
+  
   correct_count = nrow(entries_in_cluster[entries_in_cluster$max == correct_max,])
   total_count = nrow(entries_in_cluster)
   
-  return(correct_count/total_count)
+  if (total_count == 0) {
+    return(NA)
+  } else {
+    return(correct_count==total_count)  
+  }
+}
+
+load_test_suite  <- function(test_suite_path) {
+  static_files <- c("information.txt", "simulation_files")
+  files <- list.files(test_suite_path)
+  test_names <- setdiff(files,static_files)
+  test_names <- c(test_names[2],test_names[3],test_names[1]) #temporary order fix
+  
+  
+  test_names %>%
+    paste(test_suite_path, ., sep = "/") %>%   # dot '.' is for each item in the list
+    lapply(., load_all_nodes_round_data)
+  
+  
+  
+}
+
+reliability_of_test <- function(table) {
+  # All combinations of rounds and cluster ids
+  all_rounds <- unique(table["round"])
+  all_cluster_ids <- unique(table["cluster_id"])
+  g <- expand.grid(t(all_rounds), t(all_cluster_ids))
+  colnames(g) <- c("round","cluster")
+  
+  #calculate completion rate for each row
+  g <- mdply(g, Curry(completion_rate, table))
+  colnames(g)[3] <- "completion_rate"
+  g <- g[complete.cases(g),] # remove NA values
+  g <- arrange(g,round)
+  
+  reliability <- mean(t(g["completion_rate"]))  
+}
+
+cluster_count <- function(table) {
+  table[["cluster_id"]] %>%
+  unique() %>%
+  length()
+}
+
+create_status_row <- function(table) {
+  c(reliability_of_test(table),cluster_count(table))  
+}
+
+red <- rgb(0.7,0.1,0.1,0.8)
+green <- rgb(0.1,0.7,0.1,0.8)
+blue <- rgb(0.1,0.1,0.7,0.8)
+
+plot_reliability_result <- function(result) {
+  ma <- max(result[1,])
+  mi <- min(result[1,])
+  range <- ma - mi
+  plot(result[1,], ylim=c(mi-range*0.1,ma+range*0.1), col=green, lwd=3, type="b", pch=20, cex=4, bty="l", ylab = "reliability", xlab = "node spread", xaxt='n')
+  
+  axis(1, at=1:3, labels=c("100", "500", "1000"))
+  #lines(result[1,]-0.00001, ylim=c(mi-range*0.1,ma+range*0.1), col=rgb(0.1,0.7,0.1,0.8), lwd=3, type="b", pch=20, cex=4, bty="l", ylab = "reliability", xlab = "competition radius")
+  #legend(x=1,y=0.5669, c("1","2","3"), col=c(green,red,blue), pch=16, title = "Competition radius")
+  legend("bottomleft",c("1","2","3"),, col=c(green,red,blue), pch=16, title = "Competition radius")
+  #text(result[1,])
+  
+}
+
+line_reliability_result <- function(result, color) {
+  ma <- max(result[1,])
+  mi <- min(result[1,])
+  range <- ma - mi
+  
+  #plot(result[1,], ylim=c(mi-range*0.1,ma+range*0.1), col=rgb(0.1,0.7,0.1,0.8), lwd=3, type="b", pch=20, cex=4, bty="l", ylab = "reliability", xlab = "node spread")
+  lines(result[1,], ylim=c(mi-range*0.1,ma+range*0.1), col=color, lwd=3, type="b", pch=20, cex=4, bty="l", ylab = "reliability", xlab = "competition radius")
+  
+  #text(result[1,])
+  
+}
+
+plot_test_suite_results <- function(tables) {
+  tables %>% reliability_of_test
 }
 
 
-all_rounds <- unique(table["rd"])
-all_cluster_ids <- unique(table["cluster_id"])
-g <- expand.grid(t(all_rounds),t(all_cluster_ids))
-colnames(g) <- c("round","cluster")
-g <- mdply(g, completion_rate)
-colnames(g)[3] <- "completion_rate"
-g <- g[complete.cases(g),]
-g <- arrange(g,round)
 
-reliability <- mean(t(g["completion_rate"]))
-print(reliability)
+test_suite_path <- paste(working_directory, "50_nodes-comp_radius_1_2018-03-29_11:41:46", sep="/")
+tables1 <- load_test_suite(test_suite_path)
+result <- sapply(tables1, create_status_row)
+plot_reliability_result(result)
+
+test_suite_path <- paste(working_directory, "50_nodes-comp_radius_2_2018-03-29_11:42:11", sep="/")
+tables2 <- load_test_suite(test_suite_path)
+result <- sapply(tables2, create_status_row)
+line_reliability_result(result, red)
+
+test_suite_path <- paste(working_directory, "50_nodes-comp_radius_3_2018-03-29_11:42:29", sep="/")
+tables3 <- load_test_suite(test_suite_path)
+result <- sapply(tables3, create_status_row)
+line_reliability_result(result, blue)
+
+
+
+
+#plot_reliability_result(result)
+#print(result)
+
+#print(reliability_of_test(table100x100))
+#print(reliability_of_test(table500x500))
+#print(reliability_of_test(table1000x1000))
