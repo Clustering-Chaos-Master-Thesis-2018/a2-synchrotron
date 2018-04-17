38579a917a2a44164092f433944e8e4e0a61f6a0
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-04-17 15:12:27 +0200

    Cluster heads should only be initiators when we are not running the clustering service

--- a2-synchrotron-contiki/core/net/mac/chaos/chaos.h
+++ a2-synchrotron-contiki/core/net/mac/chaos/chaos.h
@@ -222,7 +222,7 @@ enum {
 #endif
 
 #if CHAOS_CLUSTER
-  #define IS_INITIATOR()  (IS_CLUSTER_HEAD() || (node_id == INITIATOR_NODE_ID && (IS_CLUSTER_JOIN() || chaos_get_cluster_id() == 0)))
+  #define IS_INITIATOR()  ((IS_CLUSTER_HEAD() && !IS_CLUSTER_JOIN()) || (node_id == INITIATOR_NODE_ID && (IS_CLUSTER_JOIN() || chaos_get_cluster_id() == 0)))
 #else
   #define IS_INITIATOR()    (node_id == INITIATOR_NODE_ID)
 #endif
