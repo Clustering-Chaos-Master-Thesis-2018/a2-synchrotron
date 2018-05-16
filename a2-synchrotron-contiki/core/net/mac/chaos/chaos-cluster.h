a0829b95ff0f857c697f6aa6d5539a3adaa4e490
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-05-16 10:24:25 +0200

    During CH rounds only the first cluster head should be initiator

--- a2-synchrotron-contiki/core/net/mac/chaos/chaos-cluster.h
+++ a2-synchrotron-contiki/core/net/mac/chaos/chaos-cluster.h
@@ -10,19 +10,26 @@ typedef uint8_t node_index_t;
 #if CHAOS_CLUSTER
     #include "cluster.h"
 
-    #define JOIN_SERVICE_RUNNING()               (is_join_round)
-    #define CLUSTER_SERVICE_RUNNING()            (is_cluster_service_running)
-    #define DEMOTE_SERVICE_RUNNING()             (is_demote_service_running)
+    #define JOIN_SERVICE_RUNNING()                  (is_join_round)
+
+    #define CLUSTER_SERVICE_RUNNING()               (is_cluster_service_running)
+    #define DEMOTE_SERVICE_RUNNING()                (is_demote_service_running)
+    #define SINGLE_INITIATOR_SERVICE_RUNNING()      (CLUSTER_SERVICE_RUNNING() || DEMOTE_SERVICE_RUNNING)
+
+
+    #define HAS_CLUSTER_ID()                        (chaos_get_cluster_id() != 0)
+    #define IS_SAME_CLUSTER(RECEIVED_CLUSTER_ID)    (RECEIVED_CLUSTER_ID == chaos_get_cluster_id() || \
+                                                     RECEIVED_CLUSTER_ID == 0 || \
+                                                    !HAS_CLUSTER_ID())
+
     #define IS_CLUSTER_HEAD_ROUND()              (chaos_get_round_number() % 2 == 0 && HAS_CLUSTER_ID() && !CLUSTER_SERVICE_RUNNING() && !DEMOTE_SERVICE_RUNNING() && !JOIN_SERVICE_RUNNING())
-    #define IS_SAME_CLUSTER(RECEIVED_CLUSTER_ID) (RECEIVED_CLUSTER_ID == chaos_get_cluster_id() \
-                                               || RECEIVED_CLUSTER_ID == 0 \
-                                               || !HAS_CLUSTER_ID())
     #define IS_CLUSTER_HEAD()                    (node_id == chaos_get_cluster_id())
-    #define HAS_CLUSTER_ID()                     (chaos_get_cluster_id() != 0)
     #define IS_FORWARDER()                       (IS_CLUSTER_HEAD_ROUND() && !IS_CLUSTER_HEAD())
 
+    #define IS_FIRST_CLUSTER_HEAD()              (chaos_get_cluster_index() == 0)
+
     // During cluster service rounds, use no offset
-    #define CLUSTER_HOP_CHANNEL_OFFSET() (CLUSTER_SERVICE_RUNNING() || IS_CLUSTER_HEAD_ROUND() ? 0 : chaos_get_cluster_index())
+    #define CLUSTER_HOP_CHANNEL_OFFSET() (SINGLE_INITIATOR_SERVICE_RUNNING() || IS_CLUSTER_HEAD_ROUND() ? 0 : chaos_get_cluster_index())
 
     extern node_id_t cluster_id;
     extern uint8_t cluster_index;
