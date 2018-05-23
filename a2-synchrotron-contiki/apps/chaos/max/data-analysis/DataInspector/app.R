94affef14b09a996a4d94b9206195489554bee5e
André Samuelsson <andresamuelsson94@gmail.com>
2018-05-23 08:39:46 +0200

    Update titles of tabs in shiny app

--- a2-synchrotron-contiki/apps/chaos/max/data-analysis/DataInspector/app.R
+++ a2-synchrotron-contiki/apps/chaos/max/data-analysis/DataInspector/app.R
@@ -13,7 +13,7 @@ shinyApp(
     #shinythemes::themeSelector(),
     navbarPage(
       #theme = "cerulean",  # <--- To use a theme, uncomment this
-      "shinythemes",
+      "Bringing Order to Chaos",
       tabPanel("Found test data",
                mainPanel(
                  h1("Collected test suits"),
@@ -21,7 +21,7 @@ shinyApp(
                  uiOutput("test_suits_radio_buttons")
                )
       ),
-      tabPanel("Navbar 2",
+      tabPanel("Application Heatmap",
                  verticalLayout(
                    textOutput("application_plot_name"),
                    plotOutput("application_plot"),
@@ -30,12 +30,12 @@ shinyApp(
                                max = 700, value = c(1, 50))
                )
       ),
-      tabPanel("Navbar 3", "This panel is intentionally left blank")
+      tabPanel("Reliability", "This panel is intentionally left blank")
     )
   ),
   server = function(input, output) {
     output$test_suits_radio_buttons <- renderUI({
-      dirs <- list.dirs("/Users/tejp/tests", recursive = F, full.names = T)
+      dirs <- list.dirs("/Users/tejp/tests", recursive = F, full.names = F)
       
       test_suites <- dirs[dirs != "Simulations"]
       file_infos <- file.info(test_suites)
@@ -44,9 +44,15 @@ shinyApp(
       radioButtons("test_suite_path", label = NULL, rownames(file_infos), selected = NULL)
     })
     
-    output$active_test_suite <- renderText(
-      input$test_suite_path
-    )
+    output$active_test_suite <- renderText({
+      dirs <- list.dirs("/Users/tejp/tests", recursive = F, full.names = T)
+      ifelse(
+        is.null(input$test_suite_path),
+        "",
+        dirs[endsWith(dirs, input$test_suite_path)]
+        )
+      
+    })
     
     output$application_plot <- renderPlot({
       tests <- testNames(input$test_suite_path)
