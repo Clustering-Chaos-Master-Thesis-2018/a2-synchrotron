59403e016fd074de24e21d8f0fd1fc5adfdd3479
André Samuelsson <andresamuelsson94@gmail.com>
2018-05-29 14:48:18 +0200

    Set labels for competition radius summation plot

--- a2-synchrotron-contiki/apps/chaos/max/data-analysis/PlotCompetitionRadiusTests.R
+++ a2-synchrotron-contiki/apps/chaos/max/data-analysis/PlotCompetitionRadiusTests.R
@@ -1,6 +1,5 @@
 source("PlotReliability.R")
 
-
 comp_1_test_suites <- c(
   file.path(evaluation_directory, "competition-radius/competition-radius-1_1_2018-05-26_17.05.14/"),
   file.path(evaluation_directory, "competition-radius/competition-radius-1_2_2018-05-27_03.07.25/"),
@@ -19,7 +18,7 @@ comp_3_test_suites <- c(
   file.path(evaluation_directory, "competition-radius/competition-radius-3_3_2018-05-28_03.40.21/")
 )
 
-competition_radius_labels <- c("Competition Radius 1", "Competition Radius 2", "Competition Radius 3")
+competition_radius_labels <- c("1", "2", "3")
 
 competition_radius_test_suites <- list(comp_1_test_suites, comp_2_test_suites, comp_3_test_suites)
 competition_radius_loaded <- lapply(competition_radius_test_suites, function(test_suite_vector) lapply(test_suite_vector, loadResultsFromTestSuitePath))
