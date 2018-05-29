8e1992cd5eb65f1995341b1e857a2d5b240bc3ba
André Samuelsson <andresamuelsson94@gmail.com>
2018-05-29 14:47:34 +0200

    Small changes to ggplot in plotreliability

--- a2-synchrotron-contiki/apps/chaos/max/data-analysis/PlotReliability.R
+++ a2-synchrotron-contiki/apps/chaos/max/data-analysis/PlotReliability.R
@@ -24,11 +24,10 @@ label_and_flatten_data <- function(test_suite_groups, group_labels) {
 }
 
 plot_reliability <- function(test_suite_groups, group_labels) {
-  
   if(length(test_suite_groups) != length(group_labels)) {
     stop("Requires same length on number of test suite groups and labels")
   }
-?r
+
   stats <- label_and_flatten_data(test_suite_groups, group_labels)
   #transform(stats)
   # Aggregate reliability for rows with same spread. Create mean and sd
@@ -40,20 +39,29 @@ plot_reliability <- function(test_suite_groups, group_labels) {
   stats <- stats[complete.cases(stats),]
   #order by spread
   stats$simulation_name <- factor(stats$simulation_name, levels = stats$simulation_name[order(unique(stats$spread))])
-  
+
   ggplot(agg) +
     geom_pointrange(
+      size=1.5,
       aes(
         simulation_name,
         reliability.mean,
         ymax=reliability.mean+reliability.sd,
         ymin=reliability.mean-reliability.sd,
         color=group
-        ),
+      ),
       position = position_dodge(width = 0.5)) +
     ylab("Reliability (Mean & Sd)") + 
-    xlab("Simulation Setup") +
-    labs(color="") +
+    xlab("Network Size (Meters)") +
+    labs(color="Competition Radius") +
     coord_cartesian(ylim = c(0,1)) +
-    theme(axis.text.x=element_text(angle=45, hjust=1), plot.margin=unit(c(1,1,1,2),"cm"))
+    coord_fixed(3) +
+    theme(
+      text = element_text(size=24),
+      axis.text.x=element_text(angle=45, hjust=1),
+      legend.justification = c(1, 1),
+      legend.position = c(0.995, 0.99),
+      plot.margin=grid::unit(c(0,0,0,0), "mm")
+    )
+
 }
